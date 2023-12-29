6f5716fee09b1fccfc7bbac623caaa98f1e369d6
Rupeshrauniyar <151705269+Rupeshrauniyar@users.noreply.github.com>
2023-12-29 16:02:12 +0545

    Update product2otp.js

--- product2otp.js
+++ product2otp.js
@@ -25,7 +25,7 @@ const otp_btn = document.getElementById('otp-btn');
 otp_btn.addEventListener('click', () => {
 if (otp_inp.value == otp_val) {
 alert("Email Verified...");
-window.location.assign("http://localhost:8080/product-userdetail2.html")
+window.location.assign("product-userdetail2.html")
 }
 else {
 alert("Invalid Otp");
